98d8881f334511316fc6578b1877f9058337c2ef
David Turner <david.turner@elastic.co>
2018-07-18 12:47:50 +0100

    Introduce deterministic task queue
    The cluster coordination layer relies on timeouts to ensure that a cluster can
    successfully form, and must also deal with concurrent activity in the cluster.
    This commit introduces some infrastructure that will help us to
    deterministically test components that use concurrency and/or timeouts.

---
+++ server/src/test/java/org/elasticsearch/cluster/coordination/RunnableUtilsTests.java
@@ -0,0 +1,47 @@
+/*
+ * Licensed to Elasticsearch under one or more contributor
+ * license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Elasticsearch licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.elasticsearch.cluster.coordination;
+
+import org.elasticsearch.test.ESTestCase;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static org.elasticsearch.cluster.coordination.RunnableUtils.labelRunnable;
+import static org.hamcrest.core.Is.is;
+
+public class RunnableUtilsTests extends ESTestCase {
+    public void testLabelRunnable() {
+        AtomicInteger counter = new AtomicInteger();
+        final Object object = new Object();
+        final String expectedMessage = "message: args " + counter + " " + object;
+
+        Runnable foo = labelRunnable(counter::incrementAndGet, "message: args {} {}", counter, object);
+
+        assertThat(foo.toString(), is(expectedMessage));
+
+        assertThat(counter.get(), is(0));
+        foo.run();
+        assertThat(counter.get(), is(1));
+        foo.run();
+        assertThat(counter.get(), is(2));
+
+        assertThat(foo.toString(), is(expectedMessage));
+    }
+}
