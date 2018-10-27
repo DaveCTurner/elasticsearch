5ef3650b4416471ae82891025220ce1da5cdc7cf
David Turner <david.turner@elastic.co>
2018-10-27 12:46:30 +0100

    Imports

--- server/src/main/java/org/elasticsearch/action/admin/cluster/bootstrap/DiscoveredNodesResponse.java
+++ server/src/main/java/org/elasticsearch/action/admin/cluster/bootstrap/DiscoveredNodesResponse.java
@@ -22,11 +22,8 @@ import org.elasticsearch.action.ActionResponse;
 import org.elasticsearch.cluster.node.DiscoveryNode;
 import org.elasticsearch.common.io.stream.StreamInput;
 import org.elasticsearch.common.io.stream.StreamOutput;
-import org.elasticsearch.common.xcontent.ToXContentObject;
-import org.elasticsearch.common.xcontent.XContentBuilder;
 
 import java.io.IOException;
-import java.util.List;
 import java.util.Set;
 
 public class DiscoveredNodesResponse extends ActionResponse {
