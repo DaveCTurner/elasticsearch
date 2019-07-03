9f083c72716489f373b0c616420ab7c3df291ddf
David Turner <david.turner@elastic.co>
2019-07-03 08:25:45 +0100

    Add benchmark

---
+++ benchmarks/src/main/java/org/elasticsearch/common/regex/RegexStartsWithBenchmark.java
@@ -0,0 +1,58 @@
+/*
+ * Licensed to Elasticsearch under one or more contributor
+ * license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Elasticsearch licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.elasticsearch.common.regex;
+
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.Warmup;
+
+import java.util.concurrent.TimeUnit;
+
+@Fork(3)
+@Warmup(iterations = 10)
+@Measurement(iterations = 10)
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
+@State(Scope.Benchmark)
+@SuppressWarnings("unused") //invoked by benchmarking framework
+public class RegexStartsWithBenchmark {
+
+    private static final String testString = "abcdefghijklmnopqrstuvwxyz";
+    private static final String[] patterns;
+
+    static {
+        patterns = new String[testString.length() + 1];
+        for (int i = 0; i <= testString.length(); i++) {
+            patterns[i] = testString.substring(0, i) + "*";
+        }
+    }
+
+    @Benchmark
+    public void performSimpleMatch() {
+        for (int i = 0; i < patterns.length; i++) {
+            Regex.simpleMatch(patterns[i], testString);
+        }
+    }
+}
