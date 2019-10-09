2ebcfcba6a30aef2eb3d16675fc6b9d48d9c5c09
David Turner <david.turner@elastic.co>
2019-10-09 11:02:18 +0100

    WIP

--- x-pack/qa/full-cluster-restore/src/test/java/org/elasticsearch/smoketest/SmokeTestRestoreClusterIT.java
+++ x-pack/qa/full-cluster-restore/src/test/java/org/elasticsearch/smoketest/SmokeTestRestoreClusterIT.java
@@ -6,9 +6,31 @@
 package org.elasticsearch.smoketest;
 
 import org.elasticsearch.client.Request;
+import org.elasticsearch.common.settings.SecureString;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.util.concurrent.ThreadContext;
 import org.elasticsearch.test.rest.ESRestTestCase;
 
+import static org.elasticsearch.xpack.test.SecuritySettingsSourceField.basicAuthHeaderValue;
+
 public class SmokeTestRestoreClusterIT extends ESRestTestCase {
+
+    @Override
+    protected Settings restClientSettings() {
+        return Settings.builder()
+            .put(ThreadContext.PREFIX + ".Authorization",
+                basicAuthHeaderValue("powerless_user", new SecureString("x-pack-test-password".toCharArray())))
+            .build();
+    }
+
+    @Override
+    protected Settings restAdminSettings() {
+        return Settings.builder()
+            .put(ThreadContext.PREFIX + ".Authorization",
+                basicAuthHeaderValue("test_admin", new SecureString("x-pack-test-password".toCharArray())))
+            .build();
+    }
+
     public void testClusterEmitsNoSmoke() throws Exception {
         assertOK(client().performRequest(new Request("GET", "/_cluster/health")));
     }
