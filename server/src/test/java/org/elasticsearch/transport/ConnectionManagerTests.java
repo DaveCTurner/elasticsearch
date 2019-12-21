de642cdcbfec2ec2d8dd05cd2aae4afce3d3177a
David Turner <david.turner@elastic.co>
2019-12-21 10:57:41 +0000

    Revert "[TEST] Mute ConnectionManagerTests.testConcurrentConnectsAndDisconnects"
    This reverts commit b89ff63eb08794ef50658569672d1d7c2d150fe1.

--- server/src/test/java/org/elasticsearch/transport/ConnectionManagerTests.java
+++ server/src/test/java/org/elasticsearch/transport/ConnectionManagerTests.java
@@ -124,7 +124,6 @@ public class ConnectionManagerTests extends ESTestCase {
         assertEquals(1, nodeDisconnectedCount.get());
     }
 
-    @AwaitsFix(bugUrl = "https://github.com/elastic/elasticsearch/issues/49903")
     public void testConcurrentConnectsAndDisconnects() throws BrokenBarrierException, InterruptedException {
         DiscoveryNode node = new DiscoveryNode("", new TransportAddress(InetAddress.getLoopbackAddress(), 0), Version.CURRENT);
         Transport.Connection connection = new TestConnect(node);
