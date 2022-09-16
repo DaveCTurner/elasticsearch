1808d3ca11ecd18608f3d34ad7c23a8c32729463
David Turner <david.turner@elastic.co>
2022-09-16 10:49:16 +0100

    nonocommit

--- server/src/test/java/org/elasticsearch/cluster/routing/allocation/FailedShardsRoutingTests.java
+++ server/src/test/java/org/elasticsearch/cluster/routing/allocation/FailedShardsRoutingTests.java
@@ -435,7 +435,7 @@ public class FailedShardsRoutingTests extends ESAllocationTestCase {
         }
     }
 
-    @TestLogging(reason = "nocommit", value = "org.elasticsearch.cluster.routing.allocation.allocator:TRACE")
+    @TestLogging(reason = "TODO", value = "org.elasticsearch.cluster.routing.allocation.allocator:TRACE")
     public void testRebalanceFailure() {
         AllocationService strategy = createAllocationService(
             Settings.builder()
