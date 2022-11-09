53272713fbd41f289f2fb1522be6bdefa44aba7d
David Turner <david.turner@elastic.co>
2022-11-09 16:57:28 +0000

    More trace

--- server/src/internalClusterTest/java/org/elasticsearch/recovery/FullRollingRestartIT.java
+++ server/src/internalClusterTest/java/org/elasticsearch/recovery/FullRollingRestartIT.java
@@ -173,7 +173,9 @@ public class FullRollingRestartIT extends ESIntegTestCase {
     }
 
     @TestLogging(reason = "nocommit", value = "org.elasticsearch.cluster.service.MasterService:TRACE," +
-                                              "org.elasticsearch.cluster.routing.allocation.allocator.DesiredBalanceShardsAllocator:TRACE")
+                                              "org.elasticsearch.cluster.routing.allocation.allocator.DesiredBalanceShardsAllocator" +
+                                              ":TRACE," +
+                                              "org.elasticsearch.cluster.routing.allocation.allocator.DesiredBalanceComputer:TRACE")
     public void testNoRebalanceOnRollingRestart() throws Exception {
         // see https://github.com/elastic/elasticsearch/issues/14387
         internalCluster().startMasterOnlyNode(Settings.EMPTY);
