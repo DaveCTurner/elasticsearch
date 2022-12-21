f5622083440160b67eeb54935c6c394f557e2f71
David Turner <david.turner@elastic.co>
2022-12-21 09:15:54 +0000

    Log on completion

--- server/src/main/java/org/elasticsearch/rest/action/admin/cluster/RestVerifyRepositoryIntegrityAction.java
+++ server/src/main/java/org/elasticsearch/rest/action/admin/cluster/RestVerifyRepositoryIntegrityAction.java
@@ -10,8 +10,11 @@ package org.elasticsearch.rest.action.admin.cluster;
 
 import org.elasticsearch.action.ActionListener;
 import org.elasticsearch.action.admin.cluster.repositories.integrity.VerifyRepositoryIntegrityAction;
+import org.elasticsearch.action.support.ListenableActionFuture;
 import org.elasticsearch.client.internal.node.NodeClient;
 import org.elasticsearch.common.Strings;
+import org.elasticsearch.logging.LogManager;
+import org.elasticsearch.logging.Logger;
 import org.elasticsearch.rest.BaseRestHandler;
 import org.elasticsearch.rest.RestRequest;
 import org.elasticsearch.rest.RestResponse;
@@ -21,9 +24,12 @@ import org.elasticsearch.tasks.TaskId;
 import java.io.IOException;
 import java.util.List;
 
+import static org.elasticsearch.core.Strings.format;
 import static org.elasticsearch.rest.RestRequest.Method.POST;
 
 public class RestVerifyRepositoryIntegrityAction extends BaseRestHandler {
+    private static final Logger logger = LogManager.getLogger(RestVerifyRepositoryIntegrityAction.class);
+
     @Override
     public List<Route> routes() {
         return List.of(new Route(POST, "/_snapshot/{repository}/_verify_integrity"));
@@ -46,13 +52,44 @@ public class RestVerifyRepositoryIntegrityAction extends BaseRestHandler {
         );
         verifyRequest.masterNodeTimeout(request.paramAsTime("master_timeout", verifyRequest.masterNodeTimeout()));
         return channel -> {
-            final var task = client.executeLocally(VerifyRepositoryIntegrityAction.INSTANCE, verifyRequest, ActionListener.noop());
+            final var taskFuture = new ListenableActionFuture<TaskId>();
+            final var task = client.executeLocally(
+                VerifyRepositoryIntegrityAction.INSTANCE,
+                verifyRequest,
+                ActionListener.wrap(
+                    ignored -> taskFuture.addListener(
+                        ActionListener.wrap(
+                            t -> logSuccess(t, verifyRequest),
+                            e -> logFailure(TaskId.EMPTY_TASK_ID, e, null, verifyRequest)
+                        )
+                    ),
+                    e1 -> taskFuture.addListener(
+                        ActionListener.wrap(
+                            t -> logFailure(t, e1, null, verifyRequest),
+                            e2 -> logFailure(TaskId.EMPTY_TASK_ID, e1, e2, verifyRequest)
+                        )
+                    )
+                )
+            );
+            final var taskId = new TaskId(client.getLocalNodeId(), task.getId());
+            taskFuture.onResponse(taskId);
             try (var builder = channel.newBuilder()) {
                 builder.startObject();
-                builder.field("task", new TaskId(client.getLocalNodeId(), task.getId()).toString());
+                builder.field("task", taskId);
                 builder.endObject();
                 channel.sendResponse(new RestResponse(RestStatus.OK, builder));
             }
         };
+    }
+
+    private void logSuccess(TaskId taskId, VerifyRepositoryIntegrityAction.Request verifyRequest) {
+        logger.debug("task [{}] completed repository verification of [{}]", taskId, verifyRequest.getRepository());
+    }
+
+    private void logFailure(TaskId taskId, Exception e1, Exception e2, VerifyRepositoryIntegrityAction.Request verifyRequest) {
+        if (e2 != null) {
+            e1.addSuppressed(e2);
+        }
+        logger.warn(format("task [{}] failed repository verification of [{}]", taskId, verifyRequest.getRepository()), e1);
     }
 }
