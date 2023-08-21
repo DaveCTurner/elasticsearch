e267297716b8ba7bcc950aeea3d154b919166042
David Turner <david.turner@elastic.co>
2023-08-21 10:22:05 +0100

    WIP

--- modules/repository-s3/src/internalClusterTest/java/org/elasticsearch/repositories/s3/S3RepositoryThirdPartyTests.java
+++ modules/repository-s3/src/internalClusterTest/java/org/elasticsearch/repositories/s3/S3RepositoryThirdPartyTests.java
@@ -7,24 +7,25 @@
  */
 package org.elasticsearch.repositories.s3;
 
-import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.AbortMultipartUploadRequest;
 import com.amazonaws.services.s3.model.GetObjectRequest;
 import com.amazonaws.services.s3.model.InitiateMultipartUploadRequest;
 
 import org.elasticsearch.action.support.PlainActionFuture;
 import org.elasticsearch.action.support.master.AcknowledgedResponse;
-import org.elasticsearch.common.blobstore.BlobPath;
+import org.elasticsearch.cluster.service.ClusterService;
 import org.elasticsearch.common.blobstore.OptionalBytesReference;
 import org.elasticsearch.common.bytes.BytesArray;
 import org.elasticsearch.common.bytes.BytesReference;
 import org.elasticsearch.common.settings.MockSecureSettings;
 import org.elasticsearch.common.settings.SecureSettings;
 import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.util.BigArrays;
+import org.elasticsearch.indices.recovery.RecoverySettings;
 import org.elasticsearch.plugins.Plugin;
+import org.elasticsearch.plugins.PluginsService;
 import org.elasticsearch.repositories.AbstractThirdPartyRepositoryTestCase;
 import org.elasticsearch.repositories.RepositoriesService;
-import org.elasticsearch.repositories.Repository;
 import org.elasticsearch.test.junit.annotations.TestLogging;
 
 import java.io.IOException;
@@ -83,26 +84,51 @@ public class S3RepositoryThirdPartyTests extends AbstractThirdPartyRepositoryTes
         assertThat(putRepositoryResponse.isAcknowledged(), equalTo(true));
     }
 
-    @TestLogging(reason="nocommit", value="org.apache.http.wire:TRACE,com.amazonaws.request:TRACE")
+    @TestLogging(reason = "nocommit", value = "org.apache.http.wire:TRACE,com.amazonaws.request:TRACE")
     public void testCompareAndExchangeCleanup() throws IOException {
-        final var repository = (S3Repository) node().injector().getInstance(RepositoriesService.class).repository(TEST_REPO_NAME);
 
-        final var blobStore = (S3BlobStore) repository.getBlobStore();
-        final var blobContainer = (S3BlobContainer) blobStore.blobContainer(repository.basePath().add(getTestName()));
-        try {
-            logger.info("--> initial CAS");
+        // construct our own repo instance so we can inject a threadpool that allows to control the passage of time
+        try (
+            var repository = new S3Repository(
+                node().injector().getInstance(RepositoriesService.class).repository(TEST_REPO_NAME).getMetadata(),
+                xContentRegistry(),
+                node().injector().getInstance(PluginsService.class).filterPlugins(S3RepositoryPlugin.class).get(0).getService(),
+                node().injector().getInstance(ClusterService.class),
+                BigArrays.NON_RECYCLING_INSTANCE,
+                new RecoverySettings(node().settings(), node().injector().getInstance(ClusterService.class).getClusterSettings())
+            )
+        ) {
+            repository.start();
 
-            assertEquals(Boolean.TRUE, PlainActionFuture.<Boolean, RuntimeException>get(future ->
-                blobContainer.compareAndSetRegister("key", bytes(), bytes((byte)1), future), 10, TimeUnit.SECONDS));
-
-            logger.info("--> initial CAS done, reading object back");
-
-            assertEquals(bytes((byte)1), PlainActionFuture.<BytesReference, RuntimeException>get(future ->
-                blobContainer.getRegister("key", future.map(OptionalBytesReference::bytesReference)), 10, TimeUnit.SECONDS));
-
-            logger.info("--> successfully read object back");
+            final var blobStore = (S3BlobStore) repository.getBlobStore();
+            final var blobContainer = (S3BlobContainer) blobStore.blobContainer(repository.basePath().add(getTestName()));
 
             try (var clientReference = blobStore.clientReference()) {
+
+                logger.info("--> initial CAS");
+
+                assertEquals(
+                    Boolean.TRUE,
+                    PlainActionFuture.<Boolean, RuntimeException>get(
+                        future -> blobContainer.compareAndSetRegister("key", bytes(), bytes((byte) 1), future),
+                        10,
+                        TimeUnit.SECONDS
+                    )
+                );
+
+                logger.info("--> initial CAS done, reading object back");
+
+                assertEquals(
+                    bytes((byte) 1),
+                    PlainActionFuture.<BytesReference, RuntimeException>get(
+                        future -> blobContainer.getRegister("key", future.map(OptionalBytesReference::bytesReference)),
+                        10,
+                        TimeUnit.SECONDS
+                    )
+                );
+
+                logger.info("--> successfully read object back");
+
                 final var client = clientReference.client();
                 final var bucketName = S3Repository.BUCKET_SETTING.get(repository.getMetadata().settings());
                 final var registerBlobPath = blobContainer.buildKey("key");
@@ -111,27 +137,39 @@ public class S3RepositoryThirdPartyTests extends AbstractThirdPartyRepositoryTes
 
                 // check we're looking at the right blob
                 assertArrayEquals(
-                    new byte[]{(byte)1},
-                    client.getObject(new GetObjectRequest(bucketName, registerBlobPath)).getObjectContent().readAllBytes());
+                    new byte[] { (byte) 1 },
+                    client.getObject(new GetObjectRequest(bucketName, registerBlobPath)).getObjectContent().readAllBytes()
+                );
 
                 logger.info("--> object exists; starting new upload");
-                final var uploadId = client.initiateMultipartUpload(new InitiateMultipartUploadRequest(
-                    bucketName,
-                    registerBlobPath)).getUploadId();
+                final var uploadId = client.initiateMultipartUpload(new InitiateMultipartUploadRequest(bucketName, registerBlobPath))
+                    .getUploadId();
 
-                assertEquals(Boolean.FALSE, PlainActionFuture.<Boolean, RuntimeException>get(future ->
-                    blobContainer.compareAndSetRegister("key", bytes((byte)1), bytes((byte)2), future), 10, TimeUnit.SECONDS));
+                assertEquals(
+                    Boolean.FALSE,
+                    PlainActionFuture.<Boolean, RuntimeException>get(
+                        future -> blobContainer.compareAndSetRegister("key", bytes((byte) 1), bytes((byte) 2), future),
+                        10,
+                        TimeUnit.SECONDS
+                    )
+                );
 
                 client.abortMultipartUpload(new AbortMultipartUploadRequest(bucketName, registerBlobPath, uploadId));
 
-                assertEquals(Boolean.TRUE, PlainActionFuture.<Boolean, RuntimeException>get(future ->
-                    blobContainer.compareAndSetRegister("key", bytes((byte)1), bytes((byte)2), future), 10, TimeUnit.SECONDS));
-            }
+                assertEquals(
+                    Boolean.TRUE,
+                    PlainActionFuture.<Boolean, RuntimeException>get(
+                        future -> blobContainer.compareAndSetRegister("key", bytes((byte) 1), bytes((byte) 2), future),
+                        10,
+                        TimeUnit.SECONDS
+                    )
+                );
 
-            logger.info("--> done");
-            fail("boom");
-        } finally {
-            blobContainer.delete();
+                logger.info("--> done");
+                fail("boom");
+            } finally {
+                blobContainer.delete();
+            }
         }
     }
 
