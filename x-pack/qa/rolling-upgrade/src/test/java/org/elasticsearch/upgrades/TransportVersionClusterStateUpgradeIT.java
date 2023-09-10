4c54edb3825f752d2e4329e14a6555dbf033b197
David Turner <david.turner@elastic.co>
2023-09-10 19:32:15 +0100

    Introduce constants

--- x-pack/qa/rolling-upgrade/src/test/java/org/elasticsearch/upgrades/TransportVersionClusterStateUpgradeIT.java
+++ x-pack/qa/rolling-upgrade/src/test/java/org/elasticsearch/upgrades/TransportVersionClusterStateUpgradeIT.java
@@ -21,7 +21,13 @@ import static org.hamcrest.Matchers.oneOf;
 
 public class TransportVersionClusterStateUpgradeIT extends AbstractUpgradeTestCase {
 
+    private static final Version VERSION_INTRODUCING_TRANSPORT_VERSIONS = Version.V_8_8_0;
+    private static final Version VERSION_INTRODUCING_NODES_VERSIONS = Version.V_8_11_0;
+    private static final TransportVersion FIRST_TRANSPORT_VERSION = TransportVersions.V_8_8_0;
+
     public void testReadsInferredTransportVersions() throws Exception {
+        assertEquals(VERSION_INTRODUCING_TRANSPORT_VERSIONS.id(), FIRST_TRANSPORT_VERSION.id());
+
         // waitUntil because the versions fixup on upgrade happens in the background so may need a retry
         assertTrue(waitUntil(() -> {
             try {
@@ -48,10 +54,10 @@ public class TransportVersionClusterStateUpgradeIT extends AbstractUpgradeTestCa
 
         switch (CLUSTER_TYPE) {
             case OLD -> {
-                if (UPGRADE_FROM_VERSION.before(Version.V_8_8_0)) {
+                if (UPGRADE_FROM_VERSION.before(VERSION_INTRODUCING_TRANSPORT_VERSIONS)) {
                     assertFalse(description, hasTransportVersions);
                     assertFalse(description, hasNodesVersions);
-                } else if (UPGRADE_FROM_VERSION.before(Version.V_8_11_0)) {
+                } else if (UPGRADE_FROM_VERSION.before(VERSION_INTRODUCING_NODES_VERSIONS)) {
                     assertTrue(description, hasTransportVersions);
                     assertFalse(description, hasNodesVersions);
                 } else {
@@ -60,9 +66,9 @@ public class TransportVersionClusterStateUpgradeIT extends AbstractUpgradeTestCa
                 }
             }
             case MIXED -> {
-                if (UPGRADE_FROM_VERSION.before(Version.V_8_8_0)) {
+                if (UPGRADE_FROM_VERSION.before(VERSION_INTRODUCING_TRANSPORT_VERSIONS)) {
                     assertFalse(description, hasTransportVersions);
-                } else if (UPGRADE_FROM_VERSION.before(Version.V_8_11_0)) {
+                } else if (UPGRADE_FROM_VERSION.before(VERSION_INTRODUCING_NODES_VERSIONS)) {
                     assertTrue(description, hasNodesVersions || hasTransportVersions);
                 } else {
                     assertFalse(description, hasTransportVersions);
@@ -76,8 +82,8 @@ public class TransportVersionClusterStateUpgradeIT extends AbstractUpgradeTestCa
         }
 
         if (hasTransportVersions) {
-            assertTrue(description, UPGRADE_FROM_VERSION.before(Version.V_8_11_0));
-            assertTrue(description, UPGRADE_FROM_VERSION.onOrAfter(Version.V_8_8_0));
+            assertTrue(description, UPGRADE_FROM_VERSION.before(VERSION_INTRODUCING_NODES_VERSIONS));
+            assertTrue(description, UPGRADE_FROM_VERSION.onOrAfter(VERSION_INTRODUCING_TRANSPORT_VERSIONS));
             assertNotEquals(description, ClusterType.UPGRADED, CLUSTER_TYPE);
 
             assertEquals(description, nodeIds.size(), clusterState.evaluateArraySize("transport_versions"));
@@ -90,14 +96,14 @@ public class TransportVersionClusterStateUpgradeIT extends AbstractUpgradeTestCa
                 assertNotNull(nodeDescription, nodeVersion);
                 if (nodeVersion.equals(Version.CURRENT)) {
                     assertEquals(nodeDescription, TransportVersion.current(), transportVersion);
-                } else if (nodeVersion.after(Version.V_8_8_0)) {
-                    assertTrue(nodeDescription, transportVersion.after(TransportVersions.V_8_8_0));
+                } else if (nodeVersion.after(VERSION_INTRODUCING_TRANSPORT_VERSIONS)) {
+                    assertTrue(nodeDescription, transportVersion.after(FIRST_TRANSPORT_VERSION));
                 } else {
-                    assertEquals(nodeDescription, TransportVersions.V_8_8_0, transportVersion);
+                    assertEquals(nodeDescription, FIRST_TRANSPORT_VERSION, transportVersion);
                 }
             }
         } else if (hasNodesVersions) {
-            assertFalse(description, UPGRADE_FROM_VERSION.before(Version.V_8_11_0) && CLUSTER_TYPE == ClusterType.OLD);
+            assertFalse(description, UPGRADE_FROM_VERSION.before(VERSION_INTRODUCING_NODES_VERSIONS) && CLUSTER_TYPE == ClusterType.OLD);
             assertEquals(description, nodeIds.size(), clusterState.evaluateArraySize("nodes_versions"));
             for (int i = 0; i < nodeIds.size(); i++) {
                 final var path = "nodes_versions." + i;
@@ -110,17 +116,17 @@ public class TransportVersionClusterStateUpgradeIT extends AbstractUpgradeTestCa
                     assertThat(
                         nodeDescription,
                         transportVersion,
-                        UPGRADE_FROM_VERSION.onOrAfter(Version.V_8_8_0)
+                        UPGRADE_FROM_VERSION.onOrAfter(VERSION_INTRODUCING_TRANSPORT_VERSIONS)
                             ? equalTo(TransportVersion.current())
-                            : oneOf(TransportVersion.current(), TransportVersions.V_8_8_0)
+                            : oneOf(TransportVersion.current(), FIRST_TRANSPORT_VERSION)
                     );
-                    if (transportVersion.equals(TransportVersions.V_8_8_0)) {
+                    if (transportVersion.equals(FIRST_TRANSPORT_VERSION)) {
                         logger.info("{} - not fixed up yet, retrying", nodeDescription);
                         // not fixed up yet
                         return false;
                     }
-                } else if (nodeVersion.after(Version.V_8_8_0)) {
-                    assertTrue(nodeDescription, TransportVersions.V_8_8_0.after(transportVersion));
+                } else if (nodeVersion.after(VERSION_INTRODUCING_TRANSPORT_VERSIONS)) {
+                    assertTrue(nodeDescription, FIRST_TRANSPORT_VERSION.after(transportVersion));
                 } else {
                     assertEquals(nodeDescription, TransportVersion.fromId(nodeVersion.id()), transportVersion);
                 }
