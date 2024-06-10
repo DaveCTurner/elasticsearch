745d5c1be99bdd31a8c51c681149eeff740dc44d
David Turner <david.turner@elastic.co>
2024-06-10 08:13:13 +0100

    Extend Murmur3HashFunctionTests
    Relates #109261

--- server/src/test/java/org/elasticsearch/cluster/routing/operation/hash/murmur3/Murmur3HashFunctionTests.java
+++ server/src/test/java/org/elasticsearch/cluster/routing/operation/hash/murmur3/Murmur3HashFunctionTests.java
@@ -8,22 +8,47 @@
 
 package org.elasticsearch.cluster.routing.operation.hash.murmur3;
 
+import com.carrotsearch.randomizedtesting.annotations.Name;
+import com.carrotsearch.randomizedtesting.annotations.ParametersFactory;
+
 import org.elasticsearch.cluster.routing.Murmur3HashFunction;
 import org.elasticsearch.test.ESTestCase;
 
+import java.util.List;
+
 public class Murmur3HashFunctionTests extends ESTestCase {
 
-    public void testKnownValues() {
-        assertHash(0x5a0cb7c3, "hell");
-        assertHash(0xd7c31989, "hello");
-        assertHash(0x22ab2984, "hello w");
-        assertHash(0xdf0ca123, "hello wo");
-        assertHash(0xe7744d61, "hello wor");
-        assertHash(0xe07db09c, "The quick brown fox jumps over the lazy dog");
-        assertHash(0x4e63d2ad, "The quick brown fox jumps over the lazy cog");
+    @ParametersFactory
+    public static Iterable<Object[]> parameters() throws Exception {
+        return List.of(
+            new Object[] { 0x00000000, "" },
+            new Object[] { 0x3fb8ba02, "h" },
+            new Object[] { 0xce30a49f, "he" },
+            new Object[] { 0x0c7cee6b, "hel" },
+            new Object[] { 0x5a0cb7c3, "hell" },
+            new Object[] { 0xd7c31989, "hello" },
+            new Object[] { 0x22ab2984, "hello w" },
+            new Object[] { 0xdf0ca123, "hello wo" },
+            new Object[] { 0xe7744d61, "hello wor" },
+            new Object[] { 0x51837ec6, "hello worl" },
+            new Object[] { 0x64b256a4, "hello world" },
+            new Object[] { 0xda1575e5, "hello world!" },
+            new Object[] { 0x8ded82be, "Γειά σου Κόσμε" },
+            new Object[] { 0x8274606c, "Supplementary planes too 🙂" },
+            new Object[] { 0xe07db09c, "The quick brown fox jumps over the lazy dog" },
+            new Object[] { 0x4e63d2ad, "The quick brown fox jumps over the lazy cog" }
+        );
     }
 
-    private static void assertHash(int expected, String stringInput) {
-        assertEquals(expected, Murmur3HashFunction.hash(stringInput));
+    private final int expectedHash;
+    private final String stringInput;
+
+    public Murmur3HashFunctionTests(@Name("expectedHash") int expectedHash, @Name("stringInput") String stringInput) {
+        this.expectedHash = expectedHash;
+        this.stringInput = stringInput;
+    }
+
+    public void testKnownValue() {
+        assertEquals(expectedHash, Murmur3HashFunction.hash(stringInput));
     }
 }
