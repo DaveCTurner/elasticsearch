5d96e6c53ec6b75236c64c59d7d2e22f5a88ce76
David Turner <david.turner@elastic.co>
2024-08-25 11:18:07 +0100

    WIP

--- x-pack/plugin/snapshot-repo-test-kit/src/internalClusterTest/java/org/elasticsearch/repositories/blobstore/testkit/integrity/RepositoryVerifyIntegrityIT.java
+++ x-pack/plugin/snapshot-repo-test-kit/src/internalClusterTest/java/org/elasticsearch/repositories/blobstore/testkit/integrity/RepositoryVerifyIntegrityIT.java
@@ -7,6 +7,7 @@
 
 package org.elasticsearch.repositories.blobstore.testkit.integrity;
 
+import org.elasticsearch.ElasticsearchException;
 import org.elasticsearch.action.support.ActionTestUtils;
 import org.elasticsearch.action.support.SubscribableListener;
 import org.elasticsearch.client.Request;
@@ -19,14 +20,21 @@ import org.elasticsearch.repositories.blobstore.RepositoryFileType;
 import org.elasticsearch.repositories.blobstore.testkit.SnapshotRepositoryTestKit;
 import org.elasticsearch.repositories.fs.FsRepository;
 import org.elasticsearch.snapshots.AbstractSnapshotIntegTestCase;
+import org.elasticsearch.tasks.Task;
 import org.elasticsearch.test.ESTestCase;
 import org.elasticsearch.test.rest.ObjectPath;
 import org.elasticsearch.test.transport.MockTransportService;
+import org.elasticsearch.test.transport.StubbableTransport;
+import org.elasticsearch.transport.TransportChannel;
+import org.elasticsearch.transport.TransportRequest;
+import org.elasticsearch.transport.TransportRequestHandler;
+import org.elasticsearch.transport.TransportResponse;
 
 import java.nio.file.Path;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import static org.elasticsearch.test.hamcrest.ElasticsearchAssertions.assertAcked;
 import static org.hamcrest.Matchers.empty;
@@ -51,108 +59,6 @@ public class RepositoryVerifyIntegrityIT extends AbstractSnapshotIntegTestCase {
             SnapshotRepositoryTestKit.class,
             MockTransportService.TestPlugin.class
         );
-    }
-
-    public void testCorruption() throws Exception {
-        final var repositoryName = randomIdentifier();
-        final var repositoryRootPath = randomRepoPath();
-
-        createRepository(repositoryName, FsRepository.TYPE, repositoryRootPath);
-
-        final var indexNames = randomList(1, 3, ESTestCase::randomIdentifier);
-        for (var indexName : indexNames) {
-            createIndexWithRandomDocs(indexName, between(1, 100));
-            flushAndRefresh(indexName);
-        }
-
-        final var snapshotNames = randomList(1, 3, ESTestCase::randomIdentifier);
-        for (var snapshotName : snapshotNames) {
-            createSnapshot(repositoryName, snapshotName, indexNames);
-        }
-
-        final Response response;
-        final Path corruptedFile;
-        final RepositoryFileType corruptedFileType;
-        try (var ignored = new BlobStoreIndexShardSnapshotsIntegritySuppressor()) {
-            corruptedFile = BlobStoreCorruptionUtils.corruptRandomFile(repositoryRootPath);
-            corruptedFileType = RepositoryFileType.getRepositoryFileType(repositoryRootPath, corruptedFile);
-            logger.info("--> corrupted file: {}", corruptedFile);
-            logger.info("--> corrupted file type: {}", corruptedFileType);
-
-            final var request = new Request("POST", "/_snapshot/" + repositoryName + "/_verify_integrity");
-            if (randomBoolean()) {
-                request.addParameter("human", null);
-            }
-            if (randomBoolean()) {
-                request.addParameter("pretty", null);
-            }
-            if (corruptedFileType == RepositoryFileType.SHARD_DATA || randomBoolean()) {
-                request.addParameter("verify_blob_contents", null);
-            }
-            response = getRestClient().performRequest(request);
-        } finally {
-            assertAcked(client().admin().cluster().prepareDeleteRepository(TEST_REQUEST_TIMEOUT, TEST_REQUEST_TIMEOUT, repositoryName));
-        }
-        assertEquals(200, response.getStatusLine().getStatusCode());
-        final var responseObjectPath = ObjectPath.createFromResponse(response);
-        logger.info("--> op: {}", responseObjectPath);
-        final var logEntryCount = responseObjectPath.evaluateArraySize("log");
-        final var anomalies = new HashSet<String>();
-        final var seenIndexNames = new HashSet<String>();
-        int fullyRestorableIndices = 0;
-        for (int i = 0; i < logEntryCount; i++) {
-            final String maybeAnomaly = responseObjectPath.evaluate("log." + i + ".anomaly");
-            if (maybeAnomaly != null) {
-                anomalies.add(maybeAnomaly);
-            } else {
-                final String indexName = responseObjectPath.evaluate("log." + i + ".index.name");
-                if (indexName != null) {
-                    assertTrue(seenIndexNames.add(indexName));
-                    assertThat(indexNames, hasItem(indexName));
-                    final int totalSnapshots = responseObjectPath.evaluate("log." + i + ".snapshots.total_count");
-                    final int restorableSnapshots = responseObjectPath.evaluate("log." + i + ".snapshots.restorable_count");
-                    if (totalSnapshots == restorableSnapshots) {
-                        fullyRestorableIndices += 1;
-                    }
-                }
-            }
-        }
-
-        assertThat(
-            fullyRestorableIndices,
-            corruptedFileType == RepositoryFileType.SHARD_GENERATION || corruptedFileType.equals(RepositoryFileType.GLOBAL_METADATA)
-                ? equalTo(indexNames.size())
-                : lessThan(indexNames.size())
-        );
-        assertThat(anomalies, not(empty()));
-        assertThat(responseObjectPath.evaluate("results.total_anomalies"), greaterThanOrEqualTo(anomalies.size()));
-        assertEquals("fail", responseObjectPath.evaluate("results.result"));
-
-        logger.info("--> anomalies: {}", anomalies);
-
-        switch (corruptedFileType) {
-            case SNAPSHOT_INFO -> {
-                anomalies.remove("failed to load snapshot info");
-            }
-            case GLOBAL_METADATA -> {
-                anomalies.remove("failed to load global metadata");
-            }
-            case INDEX_METADATA -> {
-                anomalies.remove("failed to load index metadata");
-            }
-            case SHARD_GENERATION -> {
-                anomalies.remove("failed to load shard generation");
-            }
-            case SHARD_SNAPSHOT_INFO -> {
-                anomalies.remove("failed to load shard snapshot");
-            }
-            case SHARD_DATA -> {
-                anomalies.remove("missing blob");
-                anomalies.remove("mismatched blob length");
-                anomalies.remove("corrupt data blob");
-            }
-        }
-        assertThat(anomalies, empty());
     }
 
     public void testSuccess() throws Exception {
@@ -266,5 +172,218 @@ public class RepositoryVerifyIntegrityIT extends AbstractSnapshotIntegTestCase {
         } finally {
             coordNodeTransportService.clearAllRules();
         }
+    }
+
+    public void testCorruption() throws Exception {
+        final var repositoryName = randomIdentifier();
+        final var repositoryRootPath = randomRepoPath();
+
+        createRepository(repositoryName, FsRepository.TYPE, repositoryRootPath);
+
+        final var indexNames = randomList(1, 3, ESTestCase::randomIdentifier);
+        for (var indexName : indexNames) {
+            createIndexWithRandomDocs(indexName, between(1, 100));
+            flushAndRefresh(indexName);
+        }
+
+        final var snapshotNames = randomList(1, 3, ESTestCase::randomIdentifier);
+        for (var snapshotName : snapshotNames) {
+            createSnapshot(repositoryName, snapshotName, indexNames);
+        }
+
+        final Response response;
+        final Path corruptedFile;
+        final RepositoryFileType corruptedFileType;
+        try (var ignored = new BlobStoreIndexShardSnapshotsIntegritySuppressor()) {
+            corruptedFile = BlobStoreCorruptionUtils.corruptRandomFile(repositoryRootPath);
+            corruptedFileType = RepositoryFileType.getRepositoryFileType(repositoryRootPath, corruptedFile);
+            logger.info("--> corrupted file: {}", corruptedFile);
+            logger.info("--> corrupted file type: {}", corruptedFileType);
+
+            final var request = new Request("POST", "/_snapshot/" + repositoryName + "/_verify_integrity");
+            if (randomBoolean()) {
+                request.addParameter("human", null);
+            }
+            if (randomBoolean()) {
+                request.addParameter("pretty", null);
+            }
+            if (corruptedFileType == RepositoryFileType.SHARD_DATA || randomBoolean()) {
+                request.addParameter("verify_blob_contents", null);
+            }
+            response = getRestClient().performRequest(request);
+        } finally {
+            assertAcked(client().admin().cluster().prepareDeleteRepository(TEST_REQUEST_TIMEOUT, TEST_REQUEST_TIMEOUT, repositoryName));
+        }
+        assertEquals(200, response.getStatusLine().getStatusCode());
+        final var responseObjectPath = ObjectPath.createFromResponse(response);
+        logger.info("--> op: {}", responseObjectPath);
+        final var logEntryCount = responseObjectPath.evaluateArraySize("log");
+        final var anomalies = new HashSet<String>();
+        final var seenIndexNames = new HashSet<String>();
+        int fullyRestorableIndices = 0;
+        for (int i = 0; i < logEntryCount; i++) {
+            final String maybeAnomaly = responseObjectPath.evaluate("log." + i + ".anomaly");
+            if (maybeAnomaly != null) {
+                anomalies.add(maybeAnomaly);
+            } else {
+                final String indexName = responseObjectPath.evaluate("log." + i + ".index.name");
+                if (indexName != null) {
+                    assertTrue(seenIndexNames.add(indexName));
+                    assertThat(indexNames, hasItem(indexName));
+                    final int totalSnapshots = responseObjectPath.evaluate("log." + i + ".snapshots.total_count");
+                    final int restorableSnapshots = responseObjectPath.evaluate("log." + i + ".snapshots.restorable_count");
+                    if (totalSnapshots == restorableSnapshots) {
+                        fullyRestorableIndices += 1;
+                    }
+                }
+            }
+        }
+
+        assertThat(
+            fullyRestorableIndices,
+            corruptedFileType == RepositoryFileType.SHARD_GENERATION || corruptedFileType.equals(RepositoryFileType.GLOBAL_METADATA)
+                ? equalTo(indexNames.size())
+                : lessThan(indexNames.size())
+        );
+        assertThat(anomalies, not(empty()));
+        assertThat(responseObjectPath.evaluate("results.total_anomalies"), greaterThanOrEqualTo(anomalies.size()));
+        assertEquals("fail", responseObjectPath.evaluate("results.result"));
+
+        logger.info("--> anomalies: {}", anomalies);
+
+        switch (corruptedFileType) {
+            case SNAPSHOT_INFO -> {
+                anomalies.remove("failed to load snapshot info");
+            }
+            case GLOBAL_METADATA -> {
+                anomalies.remove("failed to load global metadata");
+            }
+            case INDEX_METADATA -> {
+                anomalies.remove("failed to load index metadata");
+            }
+            case SHARD_GENERATION -> {
+                anomalies.remove("failed to load shard generation");
+            }
+            case SHARD_SNAPSHOT_INFO -> {
+                anomalies.remove("failed to load shard snapshot");
+            }
+            case SHARD_DATA -> {
+                anomalies.remove("missing blob");
+                anomalies.remove("mismatched blob length");
+                anomalies.remove("corrupt data blob");
+            }
+        }
+        assertThat(anomalies, empty());
+    }
+
+    public void testTransportException() throws Exception {
+        final var repositoryName = randomIdentifier();
+        final var repositoryRootPath = randomRepoPath();
+
+        createRepository(repositoryName, FsRepository.TYPE, repositoryRootPath);
+
+        final var indexNames = randomList(1, 3, ESTestCase::randomIdentifier);
+        for (var indexName : indexNames) {
+            createIndexWithRandomDocs(indexName, between(1, 100));
+            flushAndRefresh(indexName);
+        }
+
+        final var snapshotNames = randomList(1, 3, ESTestCase::randomIdentifier);
+        for (var snapshotName : snapshotNames) {
+            createSnapshot(repositoryName, snapshotName, indexNames);
+        }
+
+        // use non-master node to coordinate the request so that we can capture chunks being sent back
+        if (internalCluster().size() == 1) {
+            internalCluster().startNode();
+        }
+        final var coordNodeName = randomValueOtherThan(internalCluster().getMasterName(), () -> internalCluster().getRandomNodeName());
+        final var coordNodeTransportService = MockTransportService.getInstance(coordNodeName);
+        final var masterTransportService = MockTransportService.getInstance(internalCluster().getMasterName());
+
+        final var messageCount = 2 // request & response
+            * (1 // forward to master
+                + 1 // start response
+                + indexNames.size() + snapshotNames.size());
+        final var failureStep = between(1, messageCount);
+
+        final var failTransportMessageBehaviour = new StubbableTransport.RequestHandlingBehavior<>() {
+            final AtomicInteger currentStep = new AtomicInteger();
+
+            @Override
+            public void messageReceived(
+                TransportRequestHandler<TransportRequest> handler,
+                TransportRequest request,
+                TransportChannel channel,
+                Task task
+            ) throws Exception {
+                if (currentStep.incrementAndGet() == failureStep) {
+                    throw new ElasticsearchException("simulated");
+                } else {
+                    handler.messageReceived(request, new TransportChannel() {
+                        @Override
+                        public String getProfileName() {
+                            return "test";
+                        }
+
+                        @Override
+                        public void sendResponse(TransportResponse response) {
+                            if (currentStep.incrementAndGet() == failureStep) {
+                                channel.sendResponse(new ElasticsearchException("simulated"));
+                            } else {
+                                channel.sendResponse(response);
+                            }
+                        }
+
+                        @Override
+                        public void sendResponse(Exception exception) {
+                            if (currentStep.incrementAndGet() == failureStep) {
+                                throw new AssertionError("shouldn't have failed yet");
+                            } else {
+                                channel.sendResponse(exception);
+                            }
+                        }
+                    }, task);
+                }
+            }
+        };
+
+        masterTransportService.addRequestHandlingBehavior(
+            TransportRepositoryVerifyIntegrityMasterNodeAction.ACTION_NAME,
+            failTransportMessageBehaviour
+        );
+
+        coordNodeTransportService.addRequestHandlingBehavior(
+            TransportRepositoryVerifyIntegrityResponseChunkAction.ACTION_NAME,
+            failTransportMessageBehaviour
+        );
+
+        final var request = new Request("POST", "/_snapshot/" + repositoryName + "/_verify_integrity");
+        if (randomBoolean()) {
+            request.addParameter("human", null);
+        }
+        if (randomBoolean()) {
+            request.addParameter("pretty", null);
+        }
+        if (failureStep <= 2) {
+            request.addParameter("ignore", "500");
+        }
+        final Response response;
+        try (var restClient = createRestClient(coordNodeName)) {
+            response = restClient.performRequest(request);
+        }
+        final var responseObjectPath = ObjectPath.createFromResponse(response);
+        if (failureStep <= 2) {
+            assertEquals(500, response.getStatusLine().getStatusCode());
+            assertNotNull(responseObjectPath.evaluate("error"));
+            assertEquals(500, (int) responseObjectPath.evaluate("status"));
+        } else {
+            assertEquals(200, response.getStatusLine().getStatusCode());
+            logger.info("--> op: {}", responseObjectPath);
+            assertNotNull(responseObjectPath.evaluate("log"));
+            assertNotNull(responseObjectPath.evaluate("exception"));
+        }
+
+        assertNull(responseObjectPath.evaluate("results"));
     }
 }
