7312218d510b4cc46f2e45e7ccce4d48c2cff1d6
David Turner <david.turner@elastic.co>
2026-03-10 14:01:04 +0000

    WIP

--- server/src/main/java/org/elasticsearch/indices/cluster/IndicesClusterStateService.java
+++ server/src/main/java/org/elasticsearch/indices/cluster/IndicesClusterStateService.java
@@ -143,7 +143,7 @@ public class IndicesClusterStateService extends AbstractLifecycleComponent imple
     private final Settings settings;
     // a list of shards that failed during recovery
     // we keep track of these shards in order to prevent repeated recovery of these shards on each cluster state update
-    // TODO need to track
+    // TODO need to track primary term here in general - write test showing the problem in main
     final ConcurrentMap<ShardId, Tuple<ShardRouting, Long>> failedShardsCache = ConcurrentCollections.newConcurrentMap();
     private final Map<ShardId, PendingShardCreation> pendingShardCreations = new HashMap<>();
     private final RepositoriesService repositoriesService;
